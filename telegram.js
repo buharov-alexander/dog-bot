cb3ab391fe3d177bcd09b9ce787e957b4325291a
buharov-alexander <buharovsasha@yandex.ru>
2020-04-05 13:05:36 +0700

    add method to send photo

--- telegram.js
+++ telegram.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
-const ENDPOINT = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+const MESSAGE_ENDPOINT = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+const PHOTO_ENDPOINT = `https://api.telegram.org/bot${TOKEN}/sendPhoto`;
 
 module.exports = {
   /**
@@ -10,8 +11,19 @@ module.exports = {
    * @param params.chat_id - chat or user ID
    */
   sendMessage(params) {
-    return axios.get(ENDPOINT, {params})
-      .then(() => console.log('Send message'))
+    return axios.get(MESSAGE_ENDPOINT, {params})
+      .then(() => console.log('Send message to bot', params))
       .catch(err => console.error(err, 'Send message error'));
+  },
+
+  /**
+   * 
+   * @param params.photo - photo
+   * @param params.chat_id - chat or user ID
+   */
+  sendPhoto(params) {
+    return axios.get(PHOTO_ENDPOINT, {params})
+      .then(() => console.log('Send photo to bot', params))
+      .catch(err => console.error(err, 'Send photo error'));
   }
 };
